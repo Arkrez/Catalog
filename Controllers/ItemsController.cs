78895b9ae2e63647c78ef4ccd77d255a1b695f00
Arkrez <draross2019@gmail.com>
2021-12-26 13:11:02 -0800

    Trying to add code

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -57,7 +57,7 @@ namespace Catalog.Controllers
             return CreatedAtAction(nameof(GetItem), new {id = item.Id}, item.AsDto());
         }
         // PUT /items/
-        [HttpPut("{id")]
+        [HttpPut("{id}")]
         public ActionResult UpdateItem(Guid id, UpdateItemDto itemDto)
         {
             var existingItem = repository.GetItem(id);
@@ -69,6 +69,17 @@ namespace Catalog.Controllers
                 Price = itemDto.Price
             };
             repository.UpdateItem(updatedItem);
+            return NoContent();
+        }
+
+        // DELETE /items/{id}
+        [HttpDelete("{id}")]
+        public ActionResult DeleteItem(Guid id)
+        {
+            var existingItem = repository.GetItem(id);
+            if (existingItem is null)
+                return NotFound();
+            repository.DeleteItem(id);
             return NoContent();
         }
     }
